80cba5bcd8e4d37bb6c1adb73e2dcf2792cee860
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-04-01 13:20:17 +0200

    better model

--- modeling/model.py
+++ modeling/model.py
@@ -1,19 +1,72 @@
 import tensorflow as tf
-from tensorflow.keras.applications import EfficientNetB1
+from tf.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tf.keras import Model
+from tf.keras.applications import EfficientNetB6
+from tf.keras import backend as K
+from tfa.optimizers import AdamW
 
 
-def build_model(dim=128):
-    """Builds and compiles a CNN model with an Adam optimizer"""
-    input = tf.keras.layers.Input(shape=(dim, dim, 3))
-    feature_extractor = EfficientNetB1(include_top=False, weights="imagenet",
-                                       input_shape=(dim, dim, 3))
-    x = feature_extractor(input)
-    x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(1, activation="sigmoid")(x)
-    model = tf.keras.Model(inputs=input, outputs=x)
+def build_model(dim=128, init_lr=3e-4, min_lr=1e-8, num_epochs=40, dropout=0.2,
+                weight_decay=5e-4, num_snapshots=5, multi_sample_dropout=True):
+    """Build model.
 
-    opt = tf.keras.optimizers.Adam(learning_rate=1e-3)
-    loss = tf.keras.losses.BinaryCrossentropy(label_smoothing=0.05)
+    Parameters
+    ----------
+    dim : int
+        Image size.
 
-    model.compile(optimizer=opt, loss=loss, metrics=["AUC"])
+    init_lr : float
+        Initial learning rate.
+
+    min_lr : float
+        Minimum learning rate in the scheduler.
+
+    num_epochs : int
+        Number of epochs.
+
+    dropout : float
+        Amount of dropout.
+
+    weight_decay : float
+        Weight decay used in AdamW.
+
+    num_snapshots : int
+        Number of warm starts for the scheduler.
+
+    multi_sample_dropout : bool
+        Enable multi sample dropout.
+
+    Returns
+    -------
+    model : tf.keras.Model
+        Compiled model.
+    """
+    backbone = EfficientNetB6(
+        include_top=False, weights="imagenet", input_shape=(dim, dim, 3))
+    dropout_layer = Dropout(dropout)
+    fc = Dense(9, activation="softmax")
+
+    x = GlobalAveragePooling2D()(backbone.output)
+    if multi_sample_dropout:
+        x = K.mean(
+            K.stack(
+                [fc(dropout_layer(x)) for _ in range(5)],
+                axis=0
+            ),
+            axis=0
+        )
+    else:
+        x = fc(dropout_layer(x))
+
+    model = Model(inputs=backbone.input, outputs=x)
+
+    scheduler = tf.keras.optimizers.schedules.CosineDecayRestarts(
+        initial_learning_rate=init_lr,
+        first_decay_steps=num_epochs / num_snapshots,
+        alpha=min_lr
+    )
+    optimizer = AdamW(weight_decay, learning_rate=scheduler)
+
+    loss = tf.keras.losses.CategoricalCrossentropy()
+    model.compile(optimizer=optimizer, loss=loss, metrics=["AUC"])
     return model
