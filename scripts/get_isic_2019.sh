0734359cc25aab2422d1f3d0087abd3b7169d91e
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-04-02 17:15:21 +0200

    better script files

--- scripts/get_isic_2019.sh
+++ scripts/get_isic_2019.sh
@@ -1,1 +1,6 @@
-wget https://www.kaggle.com/datasets/cdeotte/isic2019-512x512/download
+kaggle datasets download -d cdeotte/isic2019-512x512
+mkdir isic2019
+mv isic2019-512x512.zip isic2019/isic2019-512x512.zip
+cd isic2019
+unzip isic2019-512x512.zip
+rm isic2019-512x512.zip
--- scripts/get_isic_2020.sh
+++ scripts/get_isic_2020.sh
@@ -1,1 +1,6 @@
-wget https://www.kaggle.com/datasets/cdeotte/melanoma-512x512/download
+kaggle datasets download -d cdeotte/melanoma-512x512
+mkdir isic2020
+mv melanoma-512x512.zip isic2020/melanoma-512x512.zip
+cd isic2020
+unzip melanoma-512x512.zip
+rm melanoma-512x512.zip
